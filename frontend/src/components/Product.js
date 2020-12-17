acd1a16194b9b5b4addac40cf9b49b127ec35023
onadj <oliver.nad@gmail.com>
2020-12-17 20:21:55 +0000

    added image, number of review, price and name of product from Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,6 +6,26 @@ const Product = ({ product }) => {
         <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`} >
                 <Card.Img src={product.image} variant='top' /></a>
+
+
+            <Card.Body>
+                <a href={`/product/${product._id}`} >
+                    <Card.Title as='div'><strong>{product.name}</strong>
+
+                    </Card.Title>
+                </a>
+
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        {product.rating} from {product.NumReviews} reviews
+                </div>
+
+                </Card.Text>
+
+                <Card.Text as='h3'>
+                    ${product.price}
+                </Card.Text>
+            </Card.Body>
         </Card>
     )
 }
